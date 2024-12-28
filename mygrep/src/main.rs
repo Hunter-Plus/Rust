968254e9d5d7b04ea4d3dff3174ab55f4fa12cf8
Hunter Zhang <PlusChang@outlook.com>
2024-12-28 17:32:15 +0800

    Optimization: 6 Splitting Code into a Library Crate

--- mygrep/src/main.rs
+++ mygrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use mygrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,43 +16,8 @@ fn main() {
         config.query, config.file_path
     );
 
-    if let Err(e) = run(config) {
+    if let Err(e) = mygrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config { query, file_path }
-    }
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("The content in the file:\n{contents}");
-
-    Ok(())
 }
